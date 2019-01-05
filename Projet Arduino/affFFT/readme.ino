7997c47c7f37bd84279e468d8167caf1c1efa80f
reviserCtricher <45556063+reviserCtricher@users.noreply.github.com>
2019-01-05 14:11:17 +0100

    Update readme.ino

--- Projet Arduino/affFFT/readme.ino
+++ Projet Arduino/affFFT/readme.ino
@@ -1,1 +1,1 @@
-Le programme marche en couple avec processFFt.pde et sert à afficher de façon esthétique les données traitées par Processing. 
+Le programme marche en couple avec processFFT.pde et sert à afficher de façon esthétique les données traitées par Processing via l'Arduino sur l'écran LED. 
